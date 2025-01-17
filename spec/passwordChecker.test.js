ee8e0bf1750a95ac056465447a01a8010dea909b
Riccardo Accardi <riccardo.accardi@student.vinci.be>
2025-01-17 16:28:45 +0100

    Create ./Spec

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,15 +1,11 @@
-import {Main} from './passwordChecker.js';
+import {Main} from '../passwordChecker.js';
 
 describe("Checking if password has 8 Char", function(){
     it("has8char : when has 0 char", function(){
         const result = Main.has8char("");
-
         expect(result).toBe(true);
+    });
 });
 
 
 
-
-
-
-});
